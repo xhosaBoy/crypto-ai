4f4b4defa96da16bdec5b5fcc94a9152fc075c4f
xhosaBoy <luyolo.nqobile@gmail.com>
2020-12-30 11:11:31 +0200

    Initial commit

---
+++ load_data.py
@@ -0,0 +1,11 @@
+from luno_python.client import Client
+
+c = Client(api_key_id='key_id', api_key_secret='key_secret')
+try:
+  res = c.get_ticker(pair='XBTZAR')
+  print(res)
+except Exception as e:
+  print(e)
+
+if __name__ == '__main__':
+    print("Hello World!")
